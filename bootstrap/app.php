459a5477e864d5a5bdc2a80faf692dc0bdd5b1c9
pajarbeuy <pajar280505@gmail.com>
2026-05-01 13:48:26 +0700

    revert: remove vercel config, back to clean laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Foundation\Application;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,7 +16,3 @@ $app = Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-
-$app->useStoragePath(env('APP_STORAGE', base_path('storage')));
-
-return $app;
